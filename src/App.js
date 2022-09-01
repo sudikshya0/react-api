776466a35d24964926a5c50533f607a651f491d7
sudikshya0 <sudikshya.rajkarnikar22@gmail.com>
2022-09-01 22:41:35 +0545

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState(null);
+  useEffect(() => {
+    const callFxn = async () => {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then((response) => {const allUsers = response?.data;
+          const last2Users = allUsers.filter((item)=> item?.id>8)
+          setUsers(last2Users);
+        })
+        .catch((err) => console.log(err));
+    };
+    callFxn();
+  }, []);
+
+  console.log("users", users);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {users ? (
+        users.map((item, index) => <div key={index}>{item.name}</div>)
+      ) : (
+        <div>loading users</div>
+      )}
     </div>
   );
 }
